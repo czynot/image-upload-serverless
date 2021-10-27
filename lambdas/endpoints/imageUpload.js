02a5dae75b9ca0417d10c65a8f2754b0d696f919
czynot <parth29dec@gmail.com>
2021-10-27 14:38:36 +0530

    Change ESM import to CJS

--- lambdas/endpoints/imageUpload.js
+++ lambdas/endpoints/imageUpload.js
@@ -1,17 +1,46 @@
-import Responses from '../common/API_Responses'; 
-import * as fileType from 'file-type';
-import { v4 as uuid } from 'uuid';
-import * as AWS from 'aws-sdk';
+const { v4: uuidv4 } = require('uuid');
+const FileType = require('file-type');
+const AWS = require('aws-sdk')
 
 const s3 = new AWS.S3()
 
 const mimes = ['image/jpeg', 'image/png', 'image/jpg'];
 
+const Responses = {
+    _200(data = {}){
+        return {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    },
+
+    _400(data = {}){
+        return {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: 400,
+            body: JSON.stringify(data)
+        }
+    },    
+};
+
 exports.handler = async event => {
     try {
+        console.log("starting execution")
+        
         const body = JSON.parse(event.body)
 
-        if (!body || !body.imagge || !body.mime) {
+        console.log(body);
+
+        if (!body || !body.image || !body.mime) {
             return Responses._400({ message: 'incorrect body on request' })
         }
 
@@ -24,8 +53,10 @@ exports.handler = async event => {
             imageData = body.image.substring(7, body.image.length)
         }
 
+        console.log("got all data")
+
         const buffer = Buffer.from(imageData, 'base64');
-        const fileInfo = await fileType.fromBuffer(buffer);
+        const fileInfo = await FileType.fromBuffer(buffer);
         const detectedExt = fileInfo.ext
         const detectedMime = fileInfo.mime
 
@@ -33,7 +64,7 @@ exports.handler = async event => {
             return Responses._400({ message: 'mime types don\'t match' });
         }
 
-        const name = uuid();
+        const name = uuidv4();
         const key = `${name}.${detectedExt}`;
 
         console.log('writing image to s3')
